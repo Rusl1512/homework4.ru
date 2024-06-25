602f2584304986157bbf382214a9c2b27a0e2c68
Rusl1512 <schubruslan@yandex.ru>
2024-06-25 20:07:36 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+My_string = input('Введите имя пользователя :')
+print(len(My_string))
+print(My_string.upper())
+print(My_string.lower())
+print(My_string.replace(' ', ''))
+print(My_string[0])
+print(My_string[-1])
+
